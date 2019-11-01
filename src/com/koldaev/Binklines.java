7f81b7b56ca706e3e1fd0ce18d7810c73e249ac0
gitroot <root>
2019-11-01 14:13:30 +0300

    поменял на актуальный адрес по свечам + некоторые доработки

--- src/com/koldaev/Binklines.java
+++ src/com/koldaev/Binklines.java
@@ -1,34 +1,55 @@
 package com.koldaev;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+
+import static java.lang.System.out;
 
 public class Binklines {
 	
 	protected static long timeInSec1;
-	protected static String url_api = "https://www.binance.com/info-api/v1/public/agg_kline?base=";
+	protected static String url_api = "https://api.binance.com/api/v1/klines?symbol=";
 	protected static String quote = "USD";
-	protected static String type = "DAY";
+	protected static String interval = "1d";
 	protected static String limit = "30";
 	
 	public static void main(String[] args) throws JSONException, IOException, InterruptedException, UnirestException {
 		if(args.length > 1) {
-			type = args[1];
-			switch(type) {
-				case "HOUR":
+			interval = args[1];
+			switch(interval) {
+				case "1h":
 					limit = "24";
 					break;
-				case "DAY":
-					limit = "30";
+				case "1d":
+					limit = "30"; 
 					break;
 			}
 		}
-		checkklines(args[0]);
+		if(args.length > 0) {
+			checkklines(args[0].toUpperCase());
+		} else {
+			out.println("необходимо задать пару");
+		}
 	}
 
-	private static void checkklines(String para) {
-		url_api += para + "quote=" + quote + "&type=" + type + "&limit=30";
+	private static void checkklines(String para) throws UnirestException {
+		url_api += para + "&interval=" + interval + "&limit=" + limit;
+		HttpResponse<JsonNode> request = Unirest.get(url_api).asJson();
+		JSONArray results = request.getBody().getArray();
+		out.println(results);
 	}
 
 }
