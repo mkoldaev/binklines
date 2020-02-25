ca0609b2ec440939cedaa8323fb4a68dbc95f0c3
root <mail@koldaev.com>
2020-02-25 13:32:04 +0300

    Планирование статистики по свечам от 5 минут до месяца

--- scripts/binstat.sh
+++ scripts/binstat.sh
@@ -2,6 +2,23 @@
 
 JAVANAME=binanceklines$1
 INTERVAL=$1
+PARASUPDATE="PARASUPDATE"
+#java -Dname=$JAVANAME -jar /scripts/binklines.jar $INTERVAL >> /scripts/$INTERVAL.log
+#/scripts/binstat.sh 15m
+#/scripts/binstat.sh 155m EOSUSDT PARASUPDATE
+
+if [ "$3" ]
+then
+   echo "$PARASUPDATE is not running. Begin runing with updating paras"
+   java -Dname=$JAVANAME -jar /scripts/binklines.jar $INTERVAL $2 $3 >> /scripts/$PARASUPDATE.log
+   exit
+fi
+
+if ps ax | grep -v grep | grep $PARASUPDATE > /dev/null
+then
+    echo "Now updating paras. Exit"
+    exit
+fi
 
 if ps ax | grep -v grep | grep $JAVANAME > /dev/null
 then
